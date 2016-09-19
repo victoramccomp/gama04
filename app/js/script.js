835f2f1bb31808b9ade0fd280c1a7eeca11adb80
Wesley Moura <wesleytmoura@gmail.com>
2016-09-19 13:04:11 -0300

    adicionado a funcao de ancorad do menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,13 @@
+/* FUNCAO DE ANCORA DO MENU */
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
+});
+/* --------------------------- */
+
+/* FUNCAO DE CADASTRO DE LEAD */
 function RegisterLead(){
 
   var name = $("#name").val();
@@ -27,7 +37,7 @@ function RegisterLead(){
       return false;
     }
 
-    if(!checkEmail(email)) {
+    if(!checkEmail(email)) { // VALIDANDO O EMAIL COM REGEX
         isValid = false;
         $("#email").addClass('error');
         $(".message").html("Esse endereço de e-mail é inválido");
@@ -43,7 +53,7 @@ function RegisterLead(){
             data: { name: name, email: email, resposta: resposta },
             success:function(data){
               var msg = jQuery.parseJSON(data);
-              
+
               if(msg.type == "true"){
                 $(".content-center").html('<div class="well well-lg text-center"><h2>'+msg.message+'</h2></div>');
               }else{
@@ -61,9 +71,13 @@ function RegisterLead(){
 
 }
 
+/* -------------------- */
 
+/* FUNCAO QUE CHECA O EMAIL VALIDO POR REGEX */
 
 function checkEmail(emailAddress) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     return pattern.test(emailAddress);
-};
+}
+
+/* --------------------- */
