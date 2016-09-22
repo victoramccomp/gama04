63909024b3f2ab084954fd998c6c0f3c80d3dba4
Wesley Moura <wesleytmoura@gmail.com>
2016-09-21 21:11:07 -0300

    escrevendo os response do fcb

--- app/js/facebook.js
+++ app/js/facebook.js
@@ -17,10 +17,10 @@ window.fbAsyncInit = function() {
 
 function loginFacebook(){
      FB.login(function(response) {
-        console.log(response);
          if (response.authResponse) {
           console.log('Welcome!  Fetching your information.... ');
           FB.api('/me', function(response) {
+            console.log(response);
             console.log('Good to see you, ' + response.name + '.');
           });
          } else {
